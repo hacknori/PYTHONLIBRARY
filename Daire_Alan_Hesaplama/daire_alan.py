60e21e8e9aa0b3701fd36aad26b7fe4b55ec1749
hacknori <41088231+hacknori@users.noreply.github.com>
2020-11-05 17:38:03 +0300

    Float Desteği
    Koddaki int kısımları float ile değiştirildi artık ondalıklı sayılar ile de çalışabilir.

--- Daire_Alan_Hesaplama/daire_alan.py
+++ Daire_Alan_Hesaplama/daire_alan.py
@@ -8,11 +8,11 @@ pi = 3.14159
 #Kullanıcıdan yarıçapı alıyoruz
 ycap=input("Dairenin yarıçapını giriniz:")
 
-#Aldığımız değeri stringden int'e çeviriyoruz ki işlem yapabilelim
-ycapint=int(cap)
+#Aldığımız değeri stringden float'a çeviriyoruz ki işlem yapabilelim
+ycapf=float(ycap)
 
 #Alan hesaplama formülünü kullanarak dairenin alanını buluyoruz
-alan=pi*(ycapint**2)
+alan=pi*(ycapf**2)
 
 #Alanı yazdırıyoruz
 print("Dairenin Alanı:",alan)
